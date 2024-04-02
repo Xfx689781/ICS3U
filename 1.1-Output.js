a8cbb6d40c8107993950eb3f4d42ae34f638700f
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-02 14:12:25 -0400

    Update 1.1-Output.js

--- 1.1-Output.js
+++ 1.1-Output.js
@@ -5,14 +5,12 @@ console.log("Cupertino, CA");
 console.log("95014");
 console.log("+1 408-996-1010");
 
-console.log("Task.02: Two Blank Lines");
-cosole.log(\n\n);
+console.log("\n\n");
 
-console.log("Task.03: Microsoft Address (in one console)");
-console.log("Microsoft Corporation"+\n+"One Microsoft Way"+\n+"Redmond, WA"+\n+"98052"+\n+"+1 425-882-8080");
+console.log("Task.02: Microsoft Address (in one console)");
+console.log("Microsoft Corporation\n"+"One Microsoft Way\n"+"Redmond, WA\n"+"98052\n"+"+1 425-882-8080\n");
 
-console.log("Task.04: Two Blank Lines");
-cosole.log(\n\n);
+console.log("\n\n");
 
-console.log("Task.05: Letters (in one console)");
-console.log("x  o  x"+\n+"o  x  o"+\n+"x  o  x");
+console.log("Task.03: Letters (in one console)");
+console.log("x  o  x\n"+"o  x  o\n"+"x  o  x\n");
