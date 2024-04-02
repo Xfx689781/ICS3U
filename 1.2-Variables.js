dbb137f6441cb0746bc5a21a7b314df2d1dc91cd
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-02 14:26:40 -0400

    Create 1.2-Variables.js

---
+++ 1.2-Variables.js
@@ -0,0 +1,7 @@
+let length = 10;
+let width = 5;
+let area = length*width;
+console.log("Part 1\n"+area+"\nArea: "+area);
+let first_name = "Santa";
+let last_name = "Claus";
+console.log("Part 2\nI still believe in "+first_name+" "+last_name);
