3f0b3cddefdc87b655dde87925338d56fac8c8c3
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-02 13:58:21 -0400

    Update and rename 1.1OutputandConsole.js to 1.1-Output.js

---
+++ 1.1-Output.js
@@ -0,0 +1,18 @@
+console.log("Task.01: Apple Address (in several console)");
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+console.log("Task.02: Two Blank Lines");
+cosole.log(\n\n);
+
+console.log("Task.03: Microsoft Address (in one console)");
+console.log("Microsoft Corporation"+\n+"One Microsoft Way"+\n+"Redmond, WA"+\n+"98052"+\n+"+1 425-882-8080");
+
+console.log("Task.04: Two Blank Lines");
+cosole.log(\n\n);
+
+console.log("Task.05: Letters (in one console)");
+console.log("x  o  x"+\n+"o  x  o"+\n+"x  o  x");
