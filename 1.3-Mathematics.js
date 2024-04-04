0858b5cd269e92dea4336b2976755bae2bd56a42
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-04 13:37:46 -0400

    Create 1.3-Mathematics.js

---
+++ 1.3-Mathematics.js
@@ -0,0 +1,20 @@
+let celsius = 100;
+let fahrenheit = 0;
+celsius += 2;
+fahrenheit = (celsius * 9/5) + 32
+console.log("Part 1: "+fahrenheit);
+console.log("\n");
+fahrenheit = 77;
+celsius = (fahrenheit - 32) * 5/9;
+console.log("Part 2: "+celsius);
+console.log("\n");
+console.log("Part 3: "+fahrenheit+" degrees fahrenheit equals to "+celsius+" degrees Celsius");
+console.log("\n");
+let x1 = -4;
+let y1 = 5;
+let x2 = 6;
+let y2 = 0;
+let m = (y2 - y1)/(x2 - x1);
+console.log("Last Part: the value of m is: "+m);
+console.log("\n");
+
