4cbf4b4054496dae2ddcdb5d08f2be412b2b3e8e
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-04 13:26:29 -0400

    Update 1.2-Variables.js

--- 1.2-Variables.js
+++ 1.2-Variables.js
@@ -3,5 +3,6 @@ let width = 5;
 let area = length*width;
 console.log("Part 1\n"+area+"\nArea: "+area);
 let first_name = "Santa";
-let last_name = "Claus";
+let last_name = "Claws";
+last_name = "Claus";
 console.log("Part 2\nI still believe in "+first_name+" "+last_name);
