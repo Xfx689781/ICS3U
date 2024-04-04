b27f3010b27ad19a25db3d81ae3b1e41e9f05bd3
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-04 13:59:32 -0400

    Update 1.1-Output.js

--- 1.1-Output.js
+++ 1.1-Output.js
@@ -1,16 +1,16 @@
-console.log("Task.01: Apple Address (in several console)");
+console.log("Task.01: Apple Address (in several console)"); //This is the first task in the first assignment, where we output the apple address in several consoles.
 console.log("Apple Inc.");
 console.log("One Apple Park Way");
 console.log("Cupertino, CA");
 console.log("95014");
 console.log("+1 408-996-1010");
 
-console.log("\n\n");
+console.log("\n\n"); //If we put \n there, a new line starts.
 
-console.log("Task.02: Microsoft Address (in one console)");
-console.log("Microsoft Corporation\n"+"One Microsoft Way\n"+"Redmond, WA\n"+"98052\n"+"+1 425-882-8080\n");
+console.log("Task.02: Microsoft Address (in one console)"); //The second task is the output of Microsoft address.
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080\n"); //We can do that in one console, where we add \n between differen lines. 
 
-console.log("\n\n");
+console.log("\n\n"); //Two new lines separate task 2 and task 3.
 
-console.log("Task.03: Letters (in one console)");
-console.log("x  o  x\n"+"o  x  o\n"+"x  o  x\n");
+console.log("Task.03: Letters (in one console)"); //In this task, we output several xs and os.
+console.log("x  o  x\n"+"o  x  o\n"+"x  o  x\n"); //Same as task 2, we use \n to get new line, instead of get many consoles.
