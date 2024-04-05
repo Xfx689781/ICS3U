25d7a2f99ac828ea1d21d17b55c629f07dfd81f9
FuxuanX689781 <159183219+Xfx689781@users.noreply.github.com>
2024-04-05 14:14:22 -0400

    Update and rename 1.2-Variables.js to 1.2-Variables.html

--- 1.2-Variables.js
+++
@@ -1,8 +0,0 @@
-let length = 10; //This is the first task in the second assignment, where we output the area of the rectangle. First of all, we assign a value to the variable "length".
-let width = 5; //We also give a value to the second variable: width.
-let area = length*width; //We assign the third variable "area" a value, which equals to length times width.
-console.log("Part 1\n"+area+"\nArea: "+area); //We output the value of area in the first line, then we output a sentence including the value of area in the second line.
-let first_name = "Santa"; //This is the second task where we assign word values. We give the variable "first_name" a word value "Santa", and we need to add quotation marks around the word value, because they are not numbers.
-let last_name = "Claws"; //We assign the value "Claws" to the variable "last_name" at first.
-last_name = "Claus"; //We give the variable "last_name" a new value: "Claus", which replaces the old value "Claws"
-console.log("Part 2\nI still believe in "+first_name+" "+last_name); //We output a full sentence, where it says "Santa Claus" instead of "Santa Claws".
